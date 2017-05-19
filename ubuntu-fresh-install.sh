9c8f01a1996bf5c601869f94ba3ccd91d9c10f90
quocbao <1412661@student.hcmus.edu.vn>
2017-05-19 10:31:22 +0700

    Update ubuntu-fresh-install.sh

--- ubuntu-fresh-install.sh
+++ ubuntu-fresh-install.sh
@@ -8,6 +8,7 @@ apt-get upgrade -y
 apt-get install -y python2.7 python3 python-pip
 apt-get install -y htop nano dnsutils git 
 apt-get install -y build-essential
-
 apt-get dist-upgrade -y
+wget -qO- https://get.docker.com/ | sh
+#usermod -aG docker docker-user
 init 6
